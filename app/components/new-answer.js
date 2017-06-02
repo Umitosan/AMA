bd55930a1b8fb6807336431dcf107bbdaf449337
Dominic Brown <yeahdom@gmail.com>
2017-06-02 16:51:56 -0700

    move answers to specific questions page

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -4,12 +4,15 @@ export default Ember.Component.extend({
   addNewAnswer: false,
   actions: {
     answerFormShow() {
+      this.set('author', '');
+      this.set('content', '');
       this.set('addNewAnswer', true);
     },
     saveAnswer() {
       var params = {
         author: this.get('author'),
-        content: this.get('content')
+        content: this.get('content'),
+        question: this.get('question')
       };
       this.set('addNewAnswer', false);
       this.sendAction('saveAnswer', params);
